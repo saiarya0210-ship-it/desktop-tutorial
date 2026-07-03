7dba2fc238ddb7e2982bd2168c7156754c33f54f
sai arya <saiarya0210@gmail.com>
2026-07-03 20:45:59 +0530

    Add files via upload

---
+++ proposal_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("❤️ Will You Be My Girlfriend? ❤️")
+
+st.write("From: Sai Arya")
+
+if st.button("Yes ❤️"):
+    st.balloons()
+    st.success("Yay! ❤️")
+
+st.button("No 😅")
+
+
